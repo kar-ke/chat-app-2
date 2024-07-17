656d1061d1824335d0b89a251a34235fbf157c07
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-07-17 18:15:20 +0530

    chat app 2 done

--- javascript/index.js
+++ javascript/index.js
@@ -16,3 +16,36 @@ navItems.forEach((navItem) => {
         navItem.classList.add('active');
     });
 });
+
+function closeContacts() {
+    const contactSection = document.querySelector('.contacts-section');
+    contactSection.classList.add('open');
+};
+
+function openContacts() {
+    const contactSection = document.querySelector('.contacts-section');
+    contactSection.classList.remove('open');
+}
+
+function openSettings() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.classList.add('open')
+}
+function closeSettings() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.classList.remove('open')
+}
+
+const contacts = document.querySelectorAll('.messeged-contact-link');
+
+contacts.forEach((contact) => {
+    contact.addEventListener('click', function () {
+        document.querySelector('.chat-section').classList.add('open-chat')
+    });
+});
+
+function backtoMain() {
+    document.querySelector('.chat-section').classList.remove('open-chat');
+};
+
+
